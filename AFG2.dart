5111fc13c7eca8629c66b1217c769b1fd8e88793
AppNewFriend <manuelkoesterde@gmail.com>
2025-03-12 14:55:20 +0000

    fertig

---
+++ AFG2.dart
@@ -0,0 +1,11 @@
+void triplePrint(String text) {
+  print(text);
+  print(text);
+  print(text);
+}
+
+void main() {
+  triplePrint("Hallo, Welt!");
+  triplePrint("Dart ist cool!");
+  triplePrint("Programmieren macht Spaß!");
+}
