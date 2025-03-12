860694ff6100c7ea6c4bf3bc006cff2cc0061547
AppNewFriend <manuelkoesterde@gmail.com>
2025-03-12 14:59:35 +0000

    fertig

---
+++ afg3.dart
@@ -0,0 +1,14 @@
+
+void reverseInitials(String firstName, String lastName) {
+    String firstInitial = firstName[firstName.length - 1];  
+      String lastInitial = lastName[lastName.length - 1];  
+
+        print("$lastInitial. $firstInitial.");
+        }
+
+        void main() {
+          reverseInitials("Max", "Mustermann");  
+            reverseInitials("Anna", "Schmidt");  
+              reverseInitials("Lukas", "Meier");  
+              }
+}
