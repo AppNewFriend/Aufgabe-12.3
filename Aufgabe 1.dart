995639e733fcf6654130f1d27ce66f9d1aad6dd1
AppNewFriend <manuelkoesterde@gmail.com>
2025-03-12 14:49:33 +0000

    fertig

---
+++ Aufgabe 1.dart
@@ -0,0 +1,10 @@
+void printNumber(int number) {
+      print(number);
+      }
+
+      void main() {
+        printNumber(10);
+          printNumber(25);
+            printNumber(42);
+            }
+}
